52a1096412c38589d67885fd811026ccf78239ae
CSComarch <rajkonkret660@gmail.com>
2023-10-04 09:51:16 +0200

    3 update matematyka.py

--- matematyka.py
+++ matematyka.py
@@ -8,5 +8,5 @@ print(math.cos(50))  # 0.9649660284921133
 
 print(math.pow(1.7 * 10 ** 308, 1))  # 1.7e+308
 print(math.pow(1.8 * 10 ** 308, 1))  # inf
-
+# 1.7 * 10 ^ 308 ^ 1
 print(math.pow(2, 10))  # 1024.0 - potęgowanie
