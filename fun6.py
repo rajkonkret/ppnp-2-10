3732ac144b30a1d8a23dfe732a801b2685b5de92
CSComarch <rajkonkret660@gmail.com>
2023-10-04 14:28:43 +0200

    3 update fun6.py

---
+++ fun6.py
@@ -0,0 +1,19 @@
+# napisz funkcję obliczjącą średnią
+
+def liczby(i=0, *cyfry):
+    print(cyfry)  # (1, 2, 3)
+    suma = 0
+    try:
+        for c in cyfry:
+            suma += c  # suma = suma + c
+        count = len(cyfry)
+        avg = suma / count
+    except Exception as e:
+        print("Bład", e)
+    else:
+        print(f"Średnia dla ucznia {i} wynosi {avg}")
+
+
+liczby()
+liczby(1, 2, 3)
+liczby(10, 2, 3, 4, 5, 6, 7, 8, 9)
