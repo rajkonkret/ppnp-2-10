96b023141a99c3aca47e40e0ed8abaf4650ff59f
CSComarch <rajkonkret660@gmail.com>
2023-10-06 15:03:11 +0200

    5 update wykres2.py.py

--- wykres1.py
+++ wykres1.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 x = [1, 2, 3, 4, 5]
 y = [2, 5, 7, 9, 11]
 plt.plot(x, y, color="red")
-plt.title("wykres liniowy")
+plt.title("Wykres liniowy")
 plt.xlabel("Oś X")
 plt.ylabel("Oś Y")
 
